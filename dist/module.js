1ffbbbf74181e49351c734a259104170c1eafac5
Nathan Lafferty <lcgamer333@gmail.com>
2018-10-19 16:58:50 -0500

    added dist to gitignore

--- dist/module.js
+++
@@ -1,5 +0,0 @@
-export { RegionForm } from "./components/region/regionform";
-export { RegionRender } from "./components/region/regionrender";
-export { RegionSummary } from "./components/settlement/regionsummary";
-export { WorldBuilder } from "./worldbuilder";
-//# sourceMappingURL=module.js.map
--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class VUtils {
-    static prettyPrintPercent(val: number): string;
-    static prettyPrintRounded(val: number): string;
-    static sum(a: number, b: number): number;
-}
